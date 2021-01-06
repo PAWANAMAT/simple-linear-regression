f49a5e2608ec7c28774a8c4313e265ccb00ef435
PAWANAMAT <64092225+PAWANAMAT@users.noreply.github.com>
2021-01-06 13:21:21 +0530

    Add files via upload

---
+++ simple linear regression.R
@@ -0,0 +1,46 @@
+library(readxl)
+Dataset <- read_excel("C:/Users/PAWAN/Downloads/Salary_Data.xlsx")
+ View(Dataset)
+
+ library(caTools)
+ set.seed(123)
+ split = sample.split(Dataset$Salary, SplitRatio = 2/3)
+ training_set = subset(Dataset, split == TRUE)
+ test_set = subset(Dataset, split == FALSE)
+ 
+ # Feature Scaling
+ # training_set = scale(training_set)
+ # test_set = scale(test_set)
+ 
+ # Fitting Simple Linear Regression to the Training set
+ regressor = lm(formula = Salary ~ YearsExperience,
+                data = training_set)
+ anova(regressor)
+ 
+ summary(regressor)
+ # Predicting the Test set results
+ y_pred = predict(regressor, newdata = test_set)
+ 
+ # Visualising the Training set results
+ library(ggplot2)
+ ggplot() +
+   geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+              colour = 'red') +
+   geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+             colour = 'blue') +
+   ggtitle('Salary vs Experience (Training set)') +
+   xlab('Years of experience') +
+   ylab('Salary')
+ 
+ # Visualising the Test set results
+ library(ggplot2)
+ ggplot() +
+   geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+              colour = 'red') +
+   geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+             colour = 'blue') +
+   ggtitle('Salary vs Experience (Test set)') +
+   xlab('Years of experience') +
+   ylab('Salary')
+ 
+ 
